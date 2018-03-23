79f040c7776f70834f5be1aea6401557fa573239
nunomendonca <nunomendonca95@gmail.com>
2018-03-23 17:26:14 +0000

    GoogleSearch integration

--- app/src/main/java/com/mei/daam/soundsync/MainActivity.java
+++ app/src/main/java/com/mei/daam/soundsync/MainActivity.java
@@ -1,22 +1,34 @@
 package com.mei.daam.soundsync;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.PlaybackEventListener { //Implements Listeners here
 
     private final static String YOUTUBEKEY = "INSERTKEYHERE";
+    private final static String SEARCHTYPE = "video";
     private final static String DEFAULTERRORMESSAGE = "Error initializing youtube";
     private YouTubePlayerView youTubePlayerView;
+    private YouTube youtube;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +36,51 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
         setContentView(R.layout.activity_main);
         youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
         youTubePlayerView.initialize(YOUTUBEKEY, this);
+        new TestSearch().execute();
+    }
+
+    private class TestSearch extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                }).build();
+
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+                search.setKey(YOUTUBEKEY);
+                search.setQ("Benfica");
+                search.setType(SEARCHTYPE);
+                search.setFields("items(id/videoId,snippet/title,snippet/thumbnails/default/url),nextPageToken");
+                search.setMaxResults((long) 3);
+
+                // Call the API and print results.
+                SearchListResponse searchResponse = search.execute();
+                List<SearchResult> searchResultList = searchResponse.getItems();
+                if (searchResultList != null) {
+                    Log.d("TAG123", "first result " + searchResultList.get(0).getId().getVideoId());
+                    Log.d("TAG123", "e-tag " + searchResultList.get(0).getEtag());
+                    Log.d("TAG123", "snippet " + searchResultList.get(0).getSnippet());
+                }
+            } catch (GoogleJsonResponseException e) {
+                Log.d("TAG123", "There was a service error: " + e.getDetails().getCode() + " : "
+                        + e.getDetails().getMessage());
+            } catch (IOException e){
+                Log.d("TAG123", "There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            } catch (Throwable t) {
+                Log.d("TAG123", "stack " + t.toString());
+            }
+            return null;
+        }
     }
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean restored) {
         if (!restored) {
-            youTubePlayer.cueVideo("azxDhcKYku4"); //https://www.youtube.com/watch?v=azxDhcKYku4
+            youTubePlayer.cueVideo("Regre_Cp2AI"); //https://www.youtube.com/watch?v=azxDhcKYku4
         }
-
     }
 
     @Override
